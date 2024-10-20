467a0a34e397f453c39ca7dec682d97e51a2dadb
Ian <westpfal6776@gmail.com>
2024-10-20 15:36:55 -0400

    Added ability to print cards upside-down

--- gameTest.py
+++ gameTest.py
@@ -2,23 +2,28 @@
 
 CARD_WIDTH = 100
 
-def draw_card(x,y,text,image_name):
-    width = CARD_WIDTH
-    height = CARD_WIDTH*1.4
-    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(x,y,width,height))
+def draw_card(x,y,text,image_name,width=CARD_WIDTH,height=CARD_WIDTH*1.4):
+    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(x,y,width,height),border_radius=7)
 
 
     pygame.font.init()  # you have to call this at the start,
     # if you want to use this module.
-    card_font = pygame.font.SysFont('Arial Bold', 60)
+    card_font = pygame.font.SysFont('Arial Bold', int(width*.6))
     card_val = card_font.render(text,False,(0,0,0))
 
-    screen.blit(card_val, (x + width/2 - 30,y + height/2-card_val.get_height()/2))
+    screen.blit(card_val, (x + width/2 - int(width*.3),y + height/2-card_val.get_height()/2))
 
     image = pygame.image.load(image_name)  # Load in image
     image = pygame.transform.scale(image,
                                (card_val.get_height() * image.get_width() / image.get_height(), card_val.get_height()))  # Scale it to whatever dimenstions you want
     screen.blit(image, (x + width/2, y + height/2 - image.get_height()/2))  # Print to the screen
+
+def draw_card_upside_down(x,y,width=CARD_WIDTH,height=CARD_WIDTH*1.4):
+    image = pygame.image.load("images/Card_back.png")
+    image = pygame.transform.scale(image,
+                                   (width,
+                                    height))  # Scale it to whatever dimenstions you want
+    screen.blit(image, (x, y))  # Print to the screen
 
 # Import and initialize the pygame library
 import pygame

284da5c533fd701eb5133872d97080c38ccf1f24
meggi <mblam@buffalo.edu>
2024-10-24 16:06:16 -0400

    pushing pygame design things from sunday

--- gameTest.py
+++ gameTest.py
@@ -59,17 +59,21 @@ while running:
     # pygame.draw.line(screen, (0,0,0), (750,0), (750, 1000))
     # pygame.draw.line(screen, (0,0,0), (0,375), (1500,375))
 
-    draw_card(640,60,"J","images/Card_spade.png")
-    draw_card(760, 60, "Q","images/Card_diamond.png")
+    draw_card_upside_down(640,60) #AI first card position
+    draw_card_upside_down(760, 60) #AI second card position
+    draw_card(640, 550, "K", "images/Card_heart.png") #Player first card position
+    draw_card(760, 550, "A", "images/Card_club.png") #Player second card position
 
-    draw_card(640, 550, "K", "images/Card_heart.png")
-    draw_card(760, 550, "A", "images/Card_club.png")
+    draw_card(460, 305, "Q", "images/Card_spade.png") #First card table position
+    draw_card(580, 305, "8", "images/Card_spade.png") #Second card table position
+    draw_card(700, 305, "5", "images/Card_spade.png") #Third card table position
+    draw_card_upside_down(820, 305) #Fourth card table position
+    draw_card_upside_down(940, 305) #Fifth card table position
 
-    draw_card(700, 305, "J", "images/Card_spade.png")
-    draw_card(820, 305, "J", "images/Card_spade.png")
-    draw_card(940, 305, "J", "images/Card_spade.png")
-    draw_card(580, 305, "J", "images/Card_spade.png")
-    draw_card(460, 305, "J", "images/Card_spade.png")
+    hori_image = pygame.image.load("images/Card_back.png")
+    hori_image = pygame.transform.scale(hori_image, (100, 140))
+    hori_image = pygame.transform.rotate(hori_image, 90)
+    screen.blit(hori_image, (150, 325))
 
     # Flip the display
     pygame.display.flip()

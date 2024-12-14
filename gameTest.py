d41734424368a34e8c5a7b5c14afc2fec8a24130
jespbel <jmbelaso@buffalo.edu>
2024-12-14 15:28:04 -0500

    Changed it so that it calls the ui

--- gameTest.py
+++ gameTest.py
@@ -1,55 +1,60 @@
 import pygame
+import display.gameButton as b
+import display.cardDisplay as c
 
-pygame.init()
+class gameTest():
+    
+    def __init__(self):
+        self.Check = b.gameButton(415, 705, 80, 40, "check")
+        self.Call = b.gameButton(615, 705, 80, 40, "call")
+        self.Raise = b.gameButton(815, 705, 80, 40, "raise")
+        self.Fold = b.gameButton(1015, 705, 80, 40, "fold")
+        
+    def getAction(self):
+        if self.Check.click == True:
+            return "check"
+        elif self.Call.click == True:
+            return "call"
+        elif self.Raise.click == True:
+            return "raise"
+        elif self.Fold.click == True:
+            return "fold"
 
-
-# Set up the drawing window
-screen = pygame.display.set_mode([1500, 750])
-
-
-# Run until the user asks to quit
-running = True
-
-while running:
-
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Fill the background with green
-    screen.fill((53, 101, 73))
-
-
-    # # Draw a solid blue circle in the center
-    # pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-
-    # give a brown border
-    pygame.draw.rect(screen, (111, 78, 55), (0,0,1500,750), 50)
-
-    # REMOVE LATER: line to indicate middle of screen
-    # pygame.draw.line(screen, (0,0,0), (750,0), (750, 1000))
-    # pygame.draw.line(screen, (0,0,0), (0,375), (1500,375))
-
-    draw_card_upside_down(640,60) #AI first card position
-    draw_card_upside_down(760, 60) #AI second card position
-    draw_card(640, 550, "K", "images/Card_heart.png") #Player first card position
-    draw_card(760, 550, "A", "images/Card_club.png") #Player second card position
-
-    draw_card(460, 305, "Q", "images/Card_spade.png") #First card table position
-    draw_card(580, 305, "8", "images/Card_spade.png") #Second card table position
-    draw_card(700, 305, "5", "images/Card_spade.png") #Third card table position
-    draw_card_upside_down(820, 305) #Fourth card table position
-    draw_card_upside_down(940, 305) #Fifth card table position
-
-    hori_image = pygame.image.load("images/Card_back.png")
-    hori_image = pygame.transform.scale(hori_image, (100, 140))
-    hori_image = pygame.transform.rotate(hori_image, 90)
-    screen.blit(hori_image, (150, 325))
-
-    # Flip the display
-    pygame.display.flip()
-
-
-# Done! Time to quit.
-pygame.quit()
+    def startDisplay(self, pot):
+    
+        screen = pygame.display.get_surface()
+        
+        if pygame.display.get_init():
+        
+            # Fill the background with green
+            screen.fill((53, 101, 73))
+            
+            # give a brown border
+            pygame.draw.rect(screen, (111, 78, 55), (0,0,1500,750), 50)
+            
+            #horizontoal card to the left side 
+            hori_image = pygame.image.load("images/Card_back.png")
+            hori_image = pygame.transform.scale(hori_image, (100, 140))
+            hori_image = pygame.transform.rotate(hori_image, 90)
+            screen.blit(hori_image, (150, 325))
+            
+            #place all of the cards in the center but with the back
+            c.draw_card(460, 305, "images/Card_back.png")
+            c.draw_card(580, 305, "images/Card_back.png")
+            c.draw_card(700, 305, "images/Card_back.png")
+            c.draw_card(820, 305, "images/Card_back.png")
+            c.draw_card(940, 305, "images/Card_back.png")
+            
+            #displays the buttons
+            self.Check.createButton()
+            self.Call.createButton()
+            self.Raise.createButton()
+            self.Fold.createButton()
+            
+            #displays the current pot of the round
+            font = pygame.font.SysFont(None, 36)
+            main_pot = font.render("The Pot: $" + str(pot), True, (255, 255, 255))
+            screen.blit(main_pot, (1245, 363))
+            
+            pygame.display.flip()
+        
